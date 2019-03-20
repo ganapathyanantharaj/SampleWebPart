25fac29c4ca99e3181516ecbc8bd9995dc82457e
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-20 13:06:26 +0530

    v4
    V4

--- src/webparts/helloWorldDemo/HelloWorldDemoWebPart.ts
+++ src/webparts/helloWorldDemo/HelloWorldDemoWebPart.ts
@@ -21,7 +21,7 @@ export default class HelloWorldDemoWebPart extends BaseClientSideWebPart<IHelloW
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint! V3 -- CI/CD</span>
+              <span class="${ styles.title }">Welcome to SharePoint! V3 GEnpact -- CI/CD</span>
               <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts for CI/CD.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
