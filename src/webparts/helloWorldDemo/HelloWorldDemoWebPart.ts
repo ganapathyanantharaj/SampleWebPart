2de88d84e4e70a6e600176100fea28c90950dbc2
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-20 11:23:56 +0530

    Title Changed
    Title Changed

--- src/webparts/helloWorldDemo/HelloWorldDemoWebPart.ts
+++ src/webparts/helloWorldDemo/HelloWorldDemoWebPart.ts
@@ -21,7 +21,7 @@ export default class HelloWorldDemoWebPart extends BaseClientSideWebPart<IHelloW
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <span class="${ styles.title }">Welcome to SharePoint! V2 -- CI/CD</span>
               <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
