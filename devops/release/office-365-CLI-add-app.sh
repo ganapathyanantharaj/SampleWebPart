888a36410b2c28dd508991b5808215a407a3c0d9
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-22 15:45:48 +0530

    Issue Fixes_1

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -1,11 +1,11 @@
-SITE_URL=$1
-EMAIL=$2
-PASS=$3
+SITE_URL=$0
+EMAIL=$1
+PASS=$2
 
 npm i -g @pnp/office365-cli 
 o365 version
 
-o365 spo login SITE_URL --authType password --userName EMAIL --password PASS
+o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
 o365 spo app add --filePath "./_SPFx build/SPFx sppkg/demowebpart.sppkg" --overwrite
 
